396c49e3fa89f0c92a88c57bf01203a815b2331c
hcy003 <chenyu19921103@gmail.com>
2015-08-19 16:45:40 +0800

    navbar + jumbotron + featured movie element

---
+++ js/logic.js
@@ -0,0 +1,26 @@
+
+// click navigation bar items to their linked page
+$("#about").on('click', function() { 
+	window.location = 'About.html'; 
+    });
+
+$("#resume").on('click', function() { 
+	window.location = 'Resume.html';
+	});
+
+$("#blogs").on('click', function() { 
+	window.location = 'Blogs.html'; 
+    });
+
+$("#中文").on('click', function() { 
+	window.location = '中文.html'; 
+    });
+
+$("#home").on('click', function() { 
+	window.location = 'Home.html'; 
+    });
+
+$("#photos").on('click', function() { 
+	window.location = 'Photos.html'; 
+    });
+
