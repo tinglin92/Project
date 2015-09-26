f4c1030a14e48a536ac4cc29541466423bd31ac0
Chenyu <huangchenyu1@gmail.com>
2015-09-26 12:56:17 +0800

    implementation of the seat booking

---
+++ js/seats.js
@@ -0,0 +1,30 @@
+/*
+* @Author: Chenyu
+* @Date:   2015-09-26 10:38:26
+* @Last Modified by:   Chenyu
+* @Last Modified time: 2015-09-26 12:45:35
+*/
+'use strict';
+var seat_selected = [];
+var index = 0;
+
+
+function selected(clicked_id) {
+	if (document.getElementById(clicked_id).classList.contains('available')) {
+			document.getElementById(clicked_id).classList.remove("available"),
+			document.getElementById(clicked_id).classList.add('booked');
+			seat_selected.push(clicked_id);
+			return;
+		}
+	if (document.getElementById(clicked_id).classList.contains('booked')) {
+			document.getElementById(clicked_id).classList.remove("booked");
+			document.getElementById(clicked_id).classList.add("available");
+			var i = seat_selected.indexOf(clicked_id);
+			seat_selected.splice(i, 1);
+			return;
+	}
+	if (document.getElementById(clicked_id).classList.contains('unavailable')) {
+			alert("This seat has already been reserved!");
+			return;
+	}
+}
