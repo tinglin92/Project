379433230f7e8611056de8ad89205821670c9933
Chenyu <chenyu19921103@gmail.com>
2015-10-05 23:40:16 +0800

    deleted hello

--- js/confirmation.js
+++ js/confirmation.js
@@ -2,9 +2,7 @@
 * @Author: Chenyu
 * @Date:   2015-09-18 14:14:15
 * @Last Modified by:   Chenyu
-* @Last Modified time: 2015-10-05 23:39:42
+* @Last Modified time: 2015-10-05 23:40:07
 */
 
 'use strict';
-
-hello
